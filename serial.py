2ee38f73cc49bb2a37cbc22dda35e86829e1794f
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-03-17 15:35:47 -0700

    Serial Generator

--- serial.py
+++ serial.py
@@ -19,4 +19,22 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create serial generator with starting value of input"""
+        self.start = start
+        self.number = start
+        self.begun = False
 
+    def generate(self):
+        """Increases the number value by one and returns it"""
+        if not self.begun:
+            self.begun = True
+            return self.number
+        else:
+            self.number += 1
+            return self.number
+
+    def reset(self):
+        """Sets the number value back to the original start value"""
+        self.number = self.start
+        self.begun = False
